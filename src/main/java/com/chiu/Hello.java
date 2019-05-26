4165c29648ab73a47ff0eb6caa3ae5da1df2b90e
m10423010 <m10423010@yuntech.org.tw>
2019-05-26 14:38:51 +0800

    Initial commit

---
+++ src/main/java/com/chiu/Hello.java
@@ -0,0 +1,9 @@
+package com.chiu;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Are you OK??");
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/java/com/chiu/Person.java
@@ -0,0 +1,8 @@
+package com.chiu;
+
+public class Person {
+    public void hello(){
+        System.out.println("Today is sunday");
+    }
+
+}
